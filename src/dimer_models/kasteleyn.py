795c718e2103446a7f63b525248f359093f13a16
Peru d'Ornellas <peru.dornellas@gmail.com>
2026-06-18 13:31:22 +0200

    fixed project structure

--- src/dimer_models/kasteleyn.py
+++ src/dimer_models/kasteleyn.py
@@ -1,22 +1,32 @@
 import ctypes
-from koala.lattice import Lattice
-from koala.flux_finder import fluxes_from_ujk, ujk_from_fluxes
+
+import mpmath
 import numpy as np
 import numpy.typing as npt
-from pfapack.ctypes import pfaffian
 import pfapack.ctypes as cpf
-from scipy import linalg as la
-import mpmath
+from koala.flux_finder import fluxes_from_ujk, ujk_from_fluxes
+from koala.lattice import Lattice
 
-def kasteleyn_matrix(lattice: Lattice, ujk: npt.NDArray, k=None):
 
-    dtype = int if k is None else complex
+def kasteleyn_matrix(lattice: Lattice, orientation: npt.NDArray, boundary_twist=None):
+    """Given an orientation, generate the corresponding Kasteleyn matrix
+
+    Args:
+        lattice (Lattice): The lattice
+        orientation (npt.NDArray): The orientation
+        boundary_twist (np.ndarray, optional): The twist to the x and y boundary. Defaults to None.
+
+    Returns:
+        np.ndarray: the Kasteleyn matrix
+    """
+
+    dtype = int if boundary_twist is None else complex
     ham = np.zeros((lattice.n_vertices, lattice.n_vertices), dtype=dtype)
 
-    hoppings = ujk.astype(int)
+    hoppings = orientation.astype(int)
 
-    if k is not None:
-        phases = np.sum(lattice.edges.crossing * k, axis=-1)
+    if boundary_twist is not None:
+        phases = np.sum(lattice.edges.crossing * boundary_twist, axis=-1)
         phases = np.exp(1j * phases)
         hoppings = hoppings * phases
 
@@ -27,165 +37,159 @@ def kasteleyn_matrix(lattice: Lattice, ujk: npt.NDArray, k=None):
     return ham
 
 
-def _pull_edge_entries(lattice, matrix):
-
-    out = np.zeros(lattice.n_edges)
-    for j, edge in enumerate(lattice.edges.indices):
-        e = np.sort(edge)
-        out[j] = matrix[tuple(e)]
-    return out
-
-
 def _fast_pfaffian(K):
     skpf10_d = cpf._init("skpf10_d")
     matrix_f = np.asarray(K, dtype=np.float64, order="F")
     result_array = (ctypes.c_double * 2)(0.0, 0.0)
-    uplo_bytes = "U".encode()
-    method_bytes = "P".encode()
+    uplo_bytes = b"U"
+    method_bytes = b"P"
     skpf10_d(K.shape[0], matrix_f, result_array, uplo_bytes, method_bytes)
     return (result_array[0], result_array[1])
+
 
 def fast_pfaffian_as_mpmath(K):
     m, e = _fast_pfaffian(K)
     return mpmath.mpf(f"{m}e{int(e)}")
 
 
+def generate_kasteleyn_orientation(lattice: Lattice):
+    """Make a valid Kasteleyn orientation and check it works
 
-def find_kasteleyn_number(lattice: Lattice, s_log=False, return_individual_sectors=False):
+    Args:
+        lattice (Lattice): The lattice
+
+    Returns:
+        np.ndarray: A 1d array of kateleyn orientations
+    """
+
+    ujk = ujk_from_fluxes(lattice, np.array([-1] * lattice.n_plaquettes))
+    assert np.all(fluxes_from_ujk(lattice, ujk) == -1)
+    return ujk
+
+
+def generate_torus_orientations(lattice: Lattice, initial_ujk: np.ndarray = None):
+    """Given a lattice, and potentially a starting kasteleyn weighting, generate the four
+    kasteleyn weightings corresponding to each PBC sector
+
+    Args:
+        lattice (Lattice): A lattice
+        initial_ujk (np.ndarray, optional): The initiall choice for a Kasteleyn orientation.
+            Defaults to None.
+
+    Returns:
+        np.ndarray: A set of four kasteleyn weightings corresponding to each PBC sector
+    """
+
+    if initial_ujk is None:
+        initial_ujk = generate_kasteleyn_orientation(lattice)
+
+    assert np.all(lattice.boundary_conditions), "This is only necessary on the torus"
+
+    x_boundary_edges = np.where(lattice.edges.crossing[:, 0] != 0)[0]
+    y_boundary_edges = np.where(lattice.edges.crossing[:, 1] != 0)[0]
+    all_ujks = np.array([initial_ujk] * 4)
+
+    all_ujks[np.ix_([1, 3], x_boundary_edges)] *= -1
+    all_ujks[np.ix_([2, 3], y_boundary_edges)] *= -1
+
+    og_flux = fluxes_from_ujk(lattice, initial_ujk)
+    assert np.all([np.all(fluxes_from_ujk(lattice, c) == og_flux) for c in all_ujks])
+
+    return all_ujks
+
+
+def find_omega(pfaffian_vals: np.ndarray):
+    """Given a set of four values (generally from Pfaffians) find the orientation of
+    omega = perm(1,1,1,-1) that maximises their weighted sum. Useful for determining
+    the right sector of the kasteleyn matrix to call K00.
+
+    Args:
+        pfaffian_vals (np.ndarray): A set our four numbers to be summed
+
+    Returns:
+        np.ndarray: Omega that maximises their sum
+    """
+
+    M = (np.ones([4, 4]) - 2 * np.eye(4)).astype(int)
+
+    choices = np.sum((M * pfaffian_vals), axis=1) / 2
+    winner = np.argmax(np.abs(choices))
+    omega = M[winner]
+    return omega
+
+
+def torus_kasteleyn_number(lattice: Lattice):
+    """Method for finding the number of dimerisations for a system in PBC
+
+    Args:
+        lattice (Lattice): The lattice
+
+    Returns:
+        mpmath.ctx_mp_python.mpf: The number of dimerisations
+    """
+    orientations = generate_torus_orientations(lattice)
+    kasteleyn_matrices = np.array([kasteleyn_matrix(lattice, o) for o in orientations])
+    pfaffians = np.array([fast_pfaffian_as_mpmath(k) for k in kasteleyn_matrices])
+    omega = find_omega(pfaffians)
+    n_dimerisations_pfaff = mpmath.nint(abs(sum(omega * pfaffians) / 2))
+    return n_dimerisations_pfaff
+
+
+def disc_kasteleyn_number(lattice: Lattice):
+    """Method for finding the number of dimerisations for a system in OBC
+
+    Args:
+        lattice (Lattice): The lattice
+
+    Returns:
+        mpmath.ctx_mp_python.mpf: The number of dimerisations
+    """
+    orientation = generate_kasteleyn_orientation(lattice)
+    k_matrix = kasteleyn_matrix(lattice, orientation)
+    pf = fast_pfaffian_as_mpmath(k_matrix)
+    return pf
+
+
+# TODO - write the disc version of this function
+def torus_dimer_correlation(
+    lattice: Lattice,
+    kasteleyn_matrices: list,
+    kasteleyn_inverses: np.ndarray,
+    kasteleyn_pfaffians: np.ndarray,
+    omega: np.ndarray,
+    edges_for_corr: np.ndarray,
+):
+
+    verts = lattice.edges.indices[edges_for_corr]
+    kasteleyn_products = np.prod(kasteleyn_matrices[:, *verts.T], axis=1)
+    slice = np.ix_(verts.flatten(), verts.flatten())
+    inverse_chunk = kasteleyn_inverses[:, *slice]
+    pfaffians_chunk = np.array([fast_pfaffian_as_mpmath(k) for k in inverse_chunk])
+    numerator = np.sum(omega * kasteleyn_pfaffians * pfaffians_chunk * kasteleyn_products)
+    inverse_denominator = np.sum(omega * kasteleyn_pfaffians)
+    probability = np.abs(numerator / inverse_denominator)
+    return probability
+
+
+##############  frontend functions  ##############
+def find_kasteleyn_number(lattice: Lattice):
     """Given a lattice, find the total number of dimerisations using Kasteleyn's method.
 
     Args:
-        lattice (Lattice): The lattice object, must have operiodic or open boundaries.
-        s_log (bool, optional): If True, returns the result as a tuple where 
-            out[0]* 10 ** out[1] is the number of dimerisations. Defaults to False.
+        lattice (Lattice): The lattice object, must have periodic or open boundaries.
 
     Raises:
-        Exception: Kasteleyn only works in periodic and open boundaries
+        ValueError: Kasteleyn only works in periodic and open boundaries
 
 
     Returns:
-        int : _description_
+        mpmath.ctx_mp_python.mpf : The number of dimerisations
     """
 
-    # open boundaries
-    if np.all(lattice.boundary_conditions == False):
-        ujk = ujk_from_fluxes(lattice, np.array([-1] * lattice.n_plaquettes))
-        assert np.all(fluxes_from_ujk(lattice, ujk) == -1)
-        K = kasteleyn_matrix(lattice, ujk)
-
-        if s_log:
-            out = _fast_pfaffian(K)
-            return (abs(out[0]), out[1])
-        else:
-            return abs(round(pfaffian(K)))
-
-    # periodic boundaries
-    elif np.all(lattice.boundary_conditions == True):
-
-        # make ujk sets
-        ujk = ujk_from_fluxes(lattice, np.array([-1] * lattice.n_plaquettes))
-        x_boundary_edges = np.where(lattice.edges.crossing[:, 0] != 0)
-        y_boundary_edges = np.where(lattice.edges.crossing[:, 1] != 0)
-        all_ujks = np.array([[ujk] * 2] * 2)
-        all_ujks[1, :, x_boundary_edges] *= -1
-        all_ujks[:, 1, y_boundary_edges] *= -1
-        ujk_list = all_ujks.reshape([-1, all_ujks.shape[-1]])
-
-        n_vals = []
-        for u in ujk_list:
-
-            # solve the pfaffians
-            assert np.all(fluxes_from_ujk(lattice, u) == -1)
-            K = kasteleyn_matrix(lattice, u)
-            if s_log:
-                n_vals.append(_fast_pfaffian(K))
-            else:
-                n_vals.append(round(pfaffian(K)))
-
-        # find the right combination to return
-        if s_log:
-            mags = np.array([x[0] for x in n_vals])
-            pows = np.array([x[1] for x in n_vals])
-
-            max_power = max(pows)
-            defecit = pows - max_power
-            mags = mags * (10 ** (defecit))
-
-            m = np.ones([4, 4]) - 2 * np.eye(4)
-            options = np.abs(np.sum(m * np.array(mags), axis=1)) / 2
-
-            return np.max(options), max_power
-
-        else:
-            m = np.ones([4, 4]) - 2 * np.eye(4)
-            options = np.abs(np.sum(m * np.array(n_vals), axis=1)) // 2
-        
-            return int(np.max(options))
-
+    # check boundaries
+    if np.all(lattice.boundary_conditions):
+        return torus_kasteleyn_number(lattice)
+    elif np.all(~lattice.boundary_conditions):
+        return disc_kasteleyn_number(lattice)
     else:
-        raise Exception("Kasteleyn only works in periodic and open boundaries")
-
-
-def find_local_dimer_probability(lattice: Lattice):
-    """Given a lattice, find the probability of each edge hosting a dimer
-
-    Args:
-        lattice (Lattice): The lattice object, must have operiodic or open boundaries.
-
-    Raises:
-        Exception: Kasteleyn only works in periodic and open boundaries
-
-
-    Returns:
-        np.ndarray: The probability of each edge hosting a dimer.
-    """
-
-    # open boundaries
-    if np.all(lattice.boundary_conditions == False):
-        ujk = ujk_from_fluxes(lattice, np.array([-1] * lattice.n_plaquettes))
-        K = kasteleyn_matrix(lattice, ujk)
-        kinv = la.inv(K)
-        return np.abs(_pull_edge_entries(lattice, kinv))
-
-    elif np.all(lattice.boundary_conditions == True):
-        # make ujk sets
-        ujk = ujk_from_fluxes(lattice, np.array([-1] * lattice.n_plaquettes))
-        x_boundary_edges = np.where(lattice.edges.crossing[:, 0] != 0)
-        y_boundary_edges = np.where(lattice.edges.crossing[:, 1] != 0)
-        all_ujks = np.array([[ujk] * 2] * 2)
-        all_ujks[1, :, x_boundary_edges] *= -1
-        all_ujks[:, 1, y_boundary_edges] *= -1
-        ujk_list = all_ujks.reshape([-1, all_ujks.shape[-1]])
-
-        # find pfaffians and inverses
-        n_mags = np.zeros(4)
-        n_pows = np.zeros(4)
-        kinv_vals = np.zeros([4, lattice.n_vertices, lattice.n_vertices])
-        for i, u in enumerate(ujk_list):
-            assert np.all(fluxes_from_ujk(lattice, u) == -1)
-            K = kasteleyn_matrix(lattice, u)
-            n_mags[i], n_pows[i] = _fast_pfaffian(K)
-            
-            if n_mags[i] == 0:
-                continue
-            
-            kinv_vals[i] = (
-                la.inv(K) * K
-            )  # I have no idea why multiplying with K works but it does...
-
-        # find the total number of dimerisations
-        max_power = max(n_pows)
-        defecit = n_pows - max_power
-        n_mags = n_mags = n_mags * (10 ** (defecit))
-        m = np.ones([4, 4]) - 2 * np.eye(4)
-        options = np.abs(np.sum(m * np.array(n_mags), axis=1))
-        w = m[np.argmax(options)]
-        total_mag = np.max(options)
-
-        # multiply probability with weight and normalise
-        inv_total = np.einsum("i, ijk, i -> jk", w, kinv_vals, n_mags)
-        result_mag = np.abs(_pull_edge_entries(lattice, inv_total)) / total_mag
-
-        return result_mag
-    else:
-        raise Exception("Kasteleyn only works in periodic and open boundaries")
+        raise ValueError("Only wotks if the latttice is in full PBC or OBC")
--- src/dimer_models/lattice_generation.py
+++ src/dimer_models/lattice_generation.py
@@ -1,12 +1,12 @@
-
-from koala.pointsets import uniform
-from koala import graph_utils as gu
-from koala.voronization import generate_lattice
-import numpy as np
-from koala.lattice import Lattice, INVALID
-from koala.graph_utils import remove_vertices
-from copy import copy
 from collections.abc import Iterable
+from copy import copy
+
+import numpy as np
+from koala import graph_utils as gu
+from koala.graph_utils import remove_vertices
+from koala.lattice import INVALID, Lattice
+from koala.pointsets import uniform
+from koala.voronization import generate_lattice
 
 
 def expand_edges(lattice: Lattice, edge_indices: np.ndarray) -> Lattice:
@@ -39,7 +39,6 @@ def expand_edges(lattice: Lattice, edge_indices: np.ndarray) -> Lattice:
     edges_to_add = []
     crossing_to_add = []
     for ed in edge_indices:
-
         # check the vertices have not already been marked for removal
         if len(np.intersect1d(vertices_for_removal, lattice.edges.indices[ed])):
             raise Exception("List of edges to remove is intersecting")
@@ -50,7 +49,6 @@ def expand_edges(lattice: Lattice, edge_indices: np.ndarray) -> Lattice:
         # now we add each new edge
         adjacent_plaqs = lattice.edges.adjacent_plaquettes[ed]
         for p in adjacent_plaqs:
-
             plaq = lattice.plaquettes[p]
             chosen_edge = np.where(plaq.edges == ed)[0][0]
 
@@ -80,34 +78,33 @@ def expand_edges(lattice: Lattice, edge_indices: np.ndarray) -> Lattice:
 
     # remove the bad vertices
     vertices_for_removal = np.concatenate(vertices_for_removal)
-    x = gu._remove_vertices_backend(
-        positions, final_edges, final_crossing, vertices_for_removal
-    )
+    x = gu._remove_vertices_backend(positions, final_edges, final_crossing, vertices_for_removal)
 
     return Lattice(*x)
 
 
-def bipartite_squarefull(n_sites, ensure_true_bipartite = True, return_pre_expanded = False):
+def bipartite_squarefull(n_sites, ensure_true_bipartite=True, return_pre_expanded=False):
 
     check = True
     while check:
-        lattice = generate_lattice(uniform(n_sites//4))
+        lattice = generate_lattice(uniform(n_sites // 4))
         dimerisation = gu.dimerise(lattice)
         lattice = gu.dimer_collapse(lattice, dimerisation)
         out = gu.vertices_to_polygon(lattice)
 
         if not ensure_true_bipartite:
             break
-        
-        x_loop_len = len(gu.find_periodic_loop(out, 'x'))
-        y_loop_len = len(gu.find_periodic_loop(out, 'y'))
-        
-        if x_loop_len%2 + y_loop_len%2 == 0:
+
+        x_loop_len = len(gu.find_periodic_loop(out, "x"))
+        y_loop_len = len(gu.find_periodic_loop(out, "y"))
+
+        if x_loop_len % 2 + y_loop_len % 2 == 0:
             check = False
 
     if return_pre_expanded:
         return out, lattice
     return out
+
 
 def find_expandable_plaquettes(lattice: Lattice):
 
@@ -165,9 +162,7 @@ def expand_plaquette_square_boundary(lattice, plaquette_id):
         starting_cross = crossing_out[starting_arg[0]][0]
         ending_cross = crossing_out[ending_arg[0]][0]
 
-        crossing_to_add = (
-            crossing_edge + sign_in * starting_cross + sign_out * ending_cross
-        )
+        crossing_to_add = crossing_edge + sign_in * starting_cross + sign_out * ending_cross
 
         edges_to_keep = np.concatenate([edges_to_keep, edge_to_add[None, :]])
         crossing_to_keep = np.concatenate([crossing_to_keep, crossing_to_add[None, :]])
@@ -177,16 +172,16 @@ def expand_plaquette_square_boundary(lattice, plaquette_id):
 
     return l_out
 
-def reduce_bipartite(lattice:Lattice, n_steps = None):
 
-    if n_steps==None: 
+def reduce_bipartite(lattice: Lattice, n_steps=None):
+
+    if n_steps is None:
         r = 1000000
     else:
         r = n_steps
     reduced_lattice = copy(lattice)
-    
-    for n in range(r):
 
+    for n in range(r):
         options = find_expandable_plaquettes(reduced_lattice)
 
         if len(options) == 0:
@@ -201,6 +196,7 @@ def reduce_bipartite(lattice:Lattice, n_steps = None):
 
     return reduced_lattice
 
+
 def expand_edges_to_squares(lattice: Lattice, chosen_edges: np.ndarray) -> Lattice:
     """Expand a select number of edges into squares
 
@@ -213,16 +209,13 @@ def expand_edges_to_squares(lattice: Lattice, chosen_edges: np.ndarray) -> Latti
     """
 
     ind_touched = lattice.edges.indices[chosen_edges].flatten()
-    assert len(set(ind_touched)) == len(
-        ind_touched
-    ), "Edges to expand touch the same vertex"
+    assert len(set(ind_touched)) == len(ind_touched), "Edges to expand touch the same vertex"
 
     if len(chosen_edges) == 0:
         return lattice
 
     # make vertices into triangles
     l2 = gu.vertices_to_polygon(lattice, lattice.edges.indices[chosen_edges].flatten())
-
 
     # next add the right edges to have the squares present
     edges_to_add = copy(l2.edges.indices)
@@ -233,16 +226,16 @@ def expand_edges_to_squares(lattice: Lattice, chosen_edges: np.ndarray) -> Latti
         adj_plaqs = l2.edges.adjacent_plaquettes[chosen_edge]
         vertices_to_remove.append(l2.edges.indices[chosen_edge])
 
-        for j,p_index in enumerate(adj_plaqs):
+        for j, p_index in enumerate(adj_plaqs):
             plaquette = l2.plaquettes[p_index]
 
             e_in_loop = np.where(plaquette.edges == chosen_edge)[0]
-            if len(e_in_loop) >1:
+            if len(e_in_loop) > 1:
                 e_in_loop = e_in_loop[j]
 
-            i_in_loop = np.array(
-                [e_in_loop - 1, e_in_loop, e_in_loop + 1]
-            ).flatten() % len(plaquette.edges)
+            i_in_loop = np.array([e_in_loop - 1, e_in_loop, e_in_loop + 1]).flatten() % len(
+                plaquette.edges
+            )
 
             signs_in_run = plaquette.directions[i_in_loop]
             edges_in_run = plaquette.edges[i_in_loop]
