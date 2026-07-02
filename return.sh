1caf6ece9f7c3e4694a4b3486912aad421feeda1
Peru d'Ornellas <peru.dornellas@gmail.com>
2026-07-02 12:41:49 +0200

    removed bloat

--- return.sh
+++
@@ -1,6 +0,0 @@
-rsync -avz --progress \
-    --exclude='.git/' \
-    --exclude='.gitignore' \
-    --exclude='.gitattributes' \
-    hyperion:/scratch/perudornellas/dimer_models/ \
-    /Users/perudornellas/python/packages/dimer_models/
--- send.sh
+++
@@ -1,6 +0,0 @@
-rsync -avz --delete \
-    --exclude='.git/' \
-    --exclude='.gitignore' \
-    --exclude='.gitattributes' \
-    /Users/perudornellas/python/packages/dimer_models/ \
-    hyperion:/scratch/perudornellas/dimer_models/
